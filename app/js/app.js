20e0649a922195f77275529625bbee42863cbdf0
Jacus1988 <121024050+Jacus1988@users.noreply.github.com>
2022-12-21 18:44:01 +0100

    Add files via upload

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+
+
+hamburger.classList.remove('active')
+mobileMenu.classList.remove('active')
+
+
+hamburger.addEventListener('click',() => {
+    
+    hamburger.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
+})
+console.log(mobileMenu);

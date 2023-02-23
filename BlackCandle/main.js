eafc2db583e596d8c793f9e6c0aa3c7e31ba6bde
Jacus1988 <121024050+Jacus1988@users.noreply.github.com>
2023-02-23 13:11:47 +0100

    Add files via upload

---
+++ BlackCandle/main.js
@@ -0,0 +1,46 @@
+/* ======= HAMBURGER ============= */
+
+const hamburger = document.querySelector('.hamburger')
+
+hamburger.addEventListener('click',() => {
+    hamburger.classList.toggle('active')
+    const list = document.querySelector('.menu')
+    list.classList.toggle('active')
+})
+
+/* ========= NAVIGATION SCROLL ============= */
+
+const nav = document.querySelector('nav')
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY>150){
+        nav.style.transition = "all 1s"
+        nav.style.boxShadow = " 0 0 10px 5px black"
+    }
+    else{
+        nav.style.boxShadow = "none"
+    }
+})
+
+/* =========== SWITCH DARK MODE ============= */
+
+const switchBtn = document.querySelector('.switch-light')
+const dark = document.querySelector('.bxs-moon')
+const light = document.querySelector('.bxs-sun')
+const body = document.querySelector('body')
+switchBtn.addEventListener('click', ()=> {
+    
+    
+    if(dark.style.display === 'none'){
+        light.style.display = "none"
+        dark.style.display = "block"
+        body.classList.remove('active')
+    }else{
+        dark.style.display = 'none';
+        light.style.display = 'block';
+        body.classList.add('active')
+
+    }
+
+    
+})

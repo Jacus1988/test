045f0026d81b6dde04e0a08d24f1782b43806d71
Jacus1988 <121024050+Jacus1988@users.noreply.github.com>
2023-02-24 10:16:48 +0100

    Add files via upload

--- BlackCandle/main.js
+++ BlackCandle/main.js
@@ -6,6 +6,11 @@ hamburger.addEventListener('click',() => {
     hamburger.classList.toggle('active')
     const list = document.querySelector('.menu')
     list.classList.toggle('active')
+
+    list.addEventListener('click',() => {
+        list.classList.remove('active')
+        hamburger.classList.remove('active')
+    })
 })
 
 /* ========= NAVIGATION SCROLL ============= */
@@ -57,4 +62,23 @@ var options = {
   
   var typed = new Typed('.sprzedajemy', options);
 
-    
+    /* ============ SECTION SELECTOR ============== */
+
+    const section = document.querySelectorAll('section')
+    const links = document.querySelectorAll('.menu li a')
+
+    window.onscroll = () => {
+        section.forEach((sec) => {
+            let top = window.scrollY
+            let offset = sec.offsetTop - 100
+            let height = sec.offsetHeight
+            let id = sec.getAttribute('id')
+
+            if(top >= offset && top < offset + height){
+                links.forEach(link => {
+                    link.classList.remove('active')
+                    document.querySelector('.menu li a[href*=' + id + ']').classList.add('active')
+                })
+            }
+        })
+    }

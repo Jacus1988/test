3d5abc98a88992aef0f98059ad567bf82d1e1ff6
Jacus1988 <121024050+Jacus1988@users.noreply.github.com>
2023-02-26 12:44:32 +0100

    Add files via upload

--- BlackCandle/main.js
+++ BlackCandle/main.js
@@ -103,6 +103,8 @@ var options = {
 
     arrow.addEventListener('click',()=>{
         window.scrollTo(0,0)
+        basket.classList.remove('active')
+        shop.style.display = 'none'
     })
 
     /* ================SCROLL REVEAL================== */
@@ -150,30 +152,29 @@ var options = {
         addToBasket()
     }
     
-    renderShop()
+    renderShop()  
     
-    
-
-    
-    
-   
-
     /* ========ADD TO BASKET ============== */
-
+    
     let productsBuyArray =[];
     const basket = document.querySelector('.basket')
+    const basketNumber = document.querySelector('.basket-number')
+    const purchaseCart = document.querySelector('.purchase-cart')
+    const purchaseCartSlots = document.querySelector('.purchase-cart-slots')
 
+    
     function addToBasket(){
         const btn = document.querySelectorAll('.item-btn')
-
+            
         btn.forEach(btn => {
             btn.addEventListener('click',() => {
                 const parent = btn.parentElement
                 productsBuyArray.push(parent)
                 basket.classList.add('active')
-                const basketNumber = document.querySelector('.basket-number')
                 basketNumber.style.display ="flex"
                 basketNumber.innerHTML = productsBuyArray.length
+                renderCartPurchase(parent)
+                basketNumber.innerHTML = purchaseCartSlots.children.length 
             })
         })
     }
@@ -195,3 +196,72 @@ var options = {
               }
              })
           })
+
+          /* ============ CLICK BASKET , RENDER SHOP CART ========== */
+
+          basket.addEventListener('click',() => {
+              purchaseCart.style.display = 'block'
+              const exitBtn = document.querySelector('.exit-btn')
+              exitBtn.addEventListener('click',()=>{
+                  purchaseCart.style.display = 'none'
+                })
+            })
+            
+            let total = [];
+            const purchaseResult = document.querySelector('.purchase-result')
+            let summary =''
+            
+
+            function renderCartPurchase(parent){
+
+                const shopItem = document.createElement('div')
+                shopItem.classList.add('purchase-item')
+                const imgItem = parent.querySelector('.item-img').src
+                const nameItem = parent.querySelector('.item-title').innerHTML
+                const priceItem = parent.querySelector('.item-price').innerHTML
+                shopItem.innerHTML = `<img src="${imgItem}" alt="" class="purchase-item-img">
+                <p class="purchase-item-name">${nameItem}</p>
+                <p class="purchase-item-quantity">szt.</p>
+                <p class="purchase-item-price">${priceItem}</p>
+                <i class='bx bxs-trash'></i>`
+
+                const delBtn = shopItem.querySelector('.bxs-trash')
+                delBtn.addEventListener('click',(e) => {
+                    const minus = parseInt(e.target.parentElement.querySelector('.purchase-item-price').innerHTML)
+                    const prevTotal = parseInt(purchaseResult.innerHTML);
+                    const newTotal = prevTotal - minus;
+                    purchaseResult.innerHTML = newTotal;
+                    console.log(minus);
+                    shopItem.remove()
+                    basketNumber.innerHTML = purchaseCartSlots.children.length
+                    if(purchaseCartSlots.children.length === 0){
+                        basketNumber.style.display = 'none'
+                    }
+                    const index = total.indexOf(minus);
+                        if (index > -1) {
+                         total.splice(index, 1);
+                         }
+                })
+                purchaseCartSlots.appendChild(shopItem)
+                
+                
+                const price = parseInt(priceItem)
+                total.push(price)
+                console.log(price);
+                
+                let totalShopCart = summaryTotalArray(total)
+                purchaseResult.innerHTML = totalShopCart 
+            }
+                
+                function summaryTotalArray (array){
+                    let suma = 0
+                    for(let i = 0; i < array.length; i++){
+                        suma += array[i]
+                    }
+                    return suma
+                }
+            
+        
+
+
+         

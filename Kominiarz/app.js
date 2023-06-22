c5150b1546dab454155c5fa99f6a44c9354bed0b
Jacus1988 <121024050+Jacus1988@users.noreply.github.com>
2023-06-22 16:14:36 +0200

    Add files via upload

--- Kominiarz/app.js
+++ Kominiarz/app.js
@@ -1,9 +1,14 @@
 const hamburger = document.querySelector('.hamburger')
-
+const header = document.querySelector('header')
 
 
 hamburger.addEventListener('click',() => {
     hamburger.classList.toggle('active')
     const mobileMenu = document.querySelector('.mobile-menu')
     mobileMenu.classList.toggle('active')
+    if(hamburger.classList.contains('active')){
+        header.style.boxShadow = "none"
+    }else{
+        header.style.boxShadow = '5px 5px 10px black';
+    }
 })
